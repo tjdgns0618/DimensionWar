df06ca09665f4023c04e8dc96abaf3101763dff7
sindek <a01040867412@gmail.com>
2024-03-28 13:34:35 +0900

    타워 스크립트 수정 및 스킬 스크립트 추가

---
+++ Assets/Scripts/Tower_Data.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tower",menuName ="Scriptble Object/Tower   Data")]
+public class Tower_Data : ScriptableObject
+{
+    public enum TowerType { Melee, Range}
+
+    [Header("# Main Info")]
+    public TowerType tower_Type;
+    public int tower_Id;
+    public string tower_Name;
+
+    [TextArea]
+    public string towerDesc;
+    public Sprite towersprite;
+
+
+    [Header("# Tower")]
+    public float baseDamage;
+    public int baseCount;
+    public float[] damages;
+    public int[] counts;
+
+
+    [Header("# Skill")]
+    public GameObject projectile;
+    public Sprite skillEffect;
+}
