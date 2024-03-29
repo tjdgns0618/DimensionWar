d8ce63448284091d367d4f061643901a1f8d83cc
tjdgns0618 <111509499+GUEMHAE@users.noreply.github.com>
2024-03-29 22:10:35 +0900

    타워머지 스크립트 오류 수정
    타워를 든 다음 자기 자리를 눌러도 합성됨 / 든 상태에서 설치 불가능한 구역 클릭시 제자리로 돌아오지 않음

--- Assets/Scripts/TowerMerge.cs
+++ Assets/Scripts/TowerMerge.cs
@@ -8,12 +8,15 @@ public class TowerMerge : MonoBehaviour
     public float surfaceHeight = 0.7f; // 카메라가 바라보는 지면의 높이를 결정합니다.
     RaycastHit hit;
     int Click = 1;
-    bool doMerge = false;
+    public bool doMerge = false;
     public int id = 1;
+    public bool canClick = true;
+    public GameObject nextTower;
 
     private void Start()
     {
-        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();        
+        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        GameManager.Instance.towers.Add(this.gameObject);
     }
 
     private void Update()
@@ -25,20 +28,35 @@ public class TowerMerge : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (Input.GetMouseButtonDown(0) && hit.transform.CompareTag("Buildable") && hit.transform.childCount == 1)
+            // 타워가 있는 블록을 클릭했을경우 타워가 있다면 클릭이 되는 코드
+            if (Input.GetMouseButtonDown(0) && hit.transform.CompareTag("Buildable") 
+                && hit.transform.childCount == 1 && canClick)
             {
                 hit.transform.GetChild(0).gameObject.GetComponent<TowerMerge>().doMerge = true;
                 hit.transform.GetChild(0).gameObject.layer = 2;
-                
+                canClick = false;
+                Debug.Log(canClick);
                 Click = 1; // 클릭됐을때 공격안하는 state로 변경
             }
-            else if (Input.GetMouseButtonDown(0) && hit.transform.GetChild(0).gameObject.GetComponent<TowerMerge>().id == this.id)
-            {
+            else if (Input.GetMouseButtonDown(0) && doMerge && 
+                hit.transform.GetChild(0).gameObject.GetComponent<TowerMerge>().id == this.id
+                && hit.transform.GetChild(0).gameObject.layer != 2)
+            { 
                 Debug.Log(hit.transform.GetChild(0).gameObject.GetComponent<TowerMerge>().id);
-                Destroy(this.gameObject);
-                Destroy(hit.transform.GetChild(0).gameObject);
-            }            
+                Debug.Log(canClick);
+                if (doMerge)
+                    Destroy(this.gameObject);
+                Destroy(hit.transform.GetChild(0).gameObject);                
+                GameObject instance = Instantiate(nextTower, hit.transform.position + new Vector3(0,surfaceHeight,0), Quaternion.Euler(15, 0, 0));
+                instance.transform.SetParent(hit.transform);
+
+                for (int i = 0; i < GameManager.Instance.towers.Count; i++)
+                {
+                    GameManager.Instance.towers[i].GetComponent<TowerMerge>().canClick = true;
+                }
+            }
         }
+        // 클릭된 타워 마우스 따라오는 코드 / 설치 불가구역 클릭시 돌아가는 코드필요함
         if (doMerge)
         {
             Vector3 targetPosition = new Vector3(hit.point.x, surfaceHeight, hit.point.z);
@@ -51,6 +69,7 @@ public class TowerMerge : MonoBehaviour
                 {
                     hit.transform.GetChild(0).gameObject.GetComponent<TowerMerge>().doMerge = false;
                     transform.localPosition = new Vector3(0, 0, 0);
+                    canClick = true;
                 }
             }
         }
